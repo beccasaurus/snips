592fb31f7e176c08cda40a4bf352f18b4bedbc42
remi <remi@remitaylor.com>
2008-01-24 02:51:49 -0700

    removed camping server and made the Rack server pass the most basic of specs

--- lib/snips/server.rb
+++ lib/snips/server.rb
@@ -6,28 +6,50 @@
 # Also contains utility methods for creating / running your own snip server(s)
 #
 class Snip::Server
-  attr_accessor :repo, :app, :adapter
+  attr_accessor :repo
 
   def initialize repo_location
     @repo = Snip::Repo.new repo_location
     raise "what in the world do you think you're doing?  i only serve local repos" unless @repo.local?
-
     require 'rack'
-    require 'snips/snip_server'
-    SnipServer::Communicator.repo = @repo
-    
-    @app     = SnipServer
-    @adapter = Rack::Adapter::Camping.new SnipServer
   end
 
   def call env
-    request = Rack::Request.new env
-    filename = request.env.PATH_INFO.sub(/^\//,'')
-    if filename[Snip::FILE_REGEX]
+    request  = Rack::Request.new env
+    path     = request.env.PATH_INFO.sub(/^\//,'')
+    compress = path[/\.Z$/]
+    response = Rack::Response.new
+    response.headers['Content-Type'] = 'text/plain'
+    response.headers['Content-Encoding'] = 'x-compress' if compress
+    require 'zlib' if compress
+
+    if    path == 'snips.index' or path == 'snips.index.Z'
+      index         = self.repo.all_snips.inject(''){ |all,snip| all << (snip.filename + "\n") }
+      response.body = (compress) ? Zlib::Deflate.deflate(index) : index
+
+    elsif path == 'snips.yaml'  or path == 'snips.yaml.Z'
+      require 'yaml'
+      yaml          = self.repo.all_snips.to_yaml
+      response.body = (compress) ? Zlib::Deflate.deflate(yaml) : yaml
+
+    elsif path[Snip::FILE_REGEX]
       return Rack::File.new( @repo.location  ).call( env )
+    
     else
-      @adapter.call env
+      return fallback( env )
+
     end
+
+    response.finish
+  end
+
+  def fallback env
+    [ 404, {'Content-Type' => 'text/plain'}, <<body ]
+Sorry, this snip server doesn't know how to handle your request.
+
+If this is your snip server, you can easily override this method to handle your request by 
+overriding Snip::Server#fallback, which accepts an argument for environment/request variables.
+body
   end
 
   def run
---
+++ spec/server_spec.rb
@@ -0,0 +1,46 @@
+require File.dirname(__FILE__) + '/spec_helper'
+require 'rack'
+require 'zlib'
+require 'yaml'
+
+describe Snip::Server do
+
+  before do
+    @server  = Snip::Server.new 'examples/snips'
+    @request = Rack::MockRequest.new @server
+  end
+
+  it 'should have access to a Snip::Repo' do
+    @server.repo.all_snips.length.should == 8
+    @server.repo.snips.length.should == 7
+  end
+
+  it 'should return the plain/text file for a snip at /#{snip filename}' do
+    response = @request.get '/test-0001.rb'
+    response.body.should == File.read( 'examples/snips/test-0001.rb' )
+  end
+
+  it 'should return a plain text index of snip names at /snips.index' do
+    response = @request.get '/snips.index'
+    @server.repo.snips.each do |snip|
+      response.body.should include( snip.filename )
+    end
+  end
+
+  it 'should return a compressed index of snip names at /snips.index.Z' do
+    response = @request.get '/snips.index.Z'
+    Zlib::Inflate.inflate( response.body ).should == @request.get( '/snips.index' ).body
+  end
+
+  it 'should return plain text yaml data for snips at /snips.yaml' do
+    response = @request.get '/snips.yaml'
+    data = YAML::load response.body
+    data.length.should == @server.repo.all_snips.length
+  end
+  
+  it 'should return compressed yaml data for snips at /snips.yaml.Z' do
+    response = @request.get '/snips.yaml.Z'
+    Zlib::Inflate.inflate( response.body ).should == @request.get( '/snips.yaml' ).body
+  end
+
+end
